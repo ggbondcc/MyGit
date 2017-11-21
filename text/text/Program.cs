ddd59e0e8930c56d21cb74b3d902bb3bf417a855
魏良俊 <ggbondcc@outlook.com>
2017-11-21 11:05:43 +0800

    first

---
+++ text/text/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>();
+            for(int i = 0;i<1000;i++)
+            {
+                a.Add(i);
+            }
+            foreach(int i in a)
+            {
+                Console.Write(i + ",");
+            }
+        }
+    }
+}
