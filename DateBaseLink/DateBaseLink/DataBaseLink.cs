f6ee845ad6955734536c112aa4d583b6bca5a5b7
魏良俊 <ggbondcc@outlook.com>
2017-11-21 17:00:06 +0800

    database class

---
+++ DateBaseLink/DateBaseLink/DataBaseLink.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows;
+
+namespace DateBaseLink
+{
+    class DataBaseLink
+    {
+        //连接数据库的字段
+        private string sqllink = "";
+        //数据库对象
+        private MySqlConnection MysqlCon;
+        private MySqlCommand MysqlCom = new MySqlCommand();
+        //通过传入数据库字符串初始化数据库连接
+        public DataBaseLink(string sqllink)
+        {
+            this.sqllink = sqllink;
+            MysqlCon = new MySqlConnection(this.sqllink);
+        }
+        //通过传入数据库对象初始化数据库连接
+        public DataBaseLink(MySqlConnection MysqlCon)
+        {
+            if(MysqlCon == null)
+            {
+                try
+                {
+                    this.MysqlCon.Open();
+                }
+                catch (MySqlException ex)
+                {
+                    Console.WriteLine(ex.ErrorCode);
+                    throw;
+                }
+                finally
+                {
+                    this.MysqlCon.Close();
+                }
+            }
+            else
+            {
+                this.MysqlCon = MysqlCon;
+            }
+        }
+        //插入数据
+        public int Insert(string SQL)
+        {
+            try
+            {
+                //MysqlCom = new MySqlCommand(SQL, MysqlCon);
+                MysqlCom.CommandText = SQL;
+                MysqlCom.Connection = MysqlCon;
+                MysqlCon.Open();
+                if(MysqlCom.ExecuteNonQuery()>0)
+                {
+                    MysqlCon.Close();
+                    return 1;
+                }
+                else
+                {
+                    MysqlCon.Close();
+                    return 0;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.ErrorCode);
+                throw;
+            }
+            
+
+            //return 0;
+        }
+        //插入数据Object替换
+        public int Insert(Object[] data ,string SQL)
+        {
+            int i = 0;
+            foreach (Object a in data)
+            {
+                SQL = SQL.Replace("{" + i + "}","'"+ a.ToString()+"'");
+                i++;
+            }
+            try
+            {
+                MysqlCom.CommandText = SQL;
+                MysqlCom.Connection = MysqlCon;
+                MysqlCon.Open();
+                //Console.WriteLine(SQL);
+                if (MysqlCom.ExecuteNonQuery() > 0)
+                {
+                    MysqlCon.Close();
+                    return 1;
+                }
+                else
+                {
+                    MysqlCon.Close();
+                    return 0;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.ErrorCode);
+                throw;
+            }
+            
+            
+        }
+        //更新数据
+        public int Updata(string SQL)
+        {
+            try
+            {
+                //MysqlCom = new MySqlCommand(SQL, MysqlCon);
+                MysqlCom.CommandText = SQL;
+                MysqlCom.Connection = MysqlCon;
+                MysqlCon.Open();
+                if (MysqlCom.ExecuteNonQuery() > 0)
+                {
+                    MysqlCon.Close();
+                    return 1;
+                }
+                else
+                {
+                    MysqlCon.Close();
+                    return 0;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.ErrorCode);
+                throw;
+            }
+            
+        }
+        //更新数据Object替换
+        public int Updata(Object[] data , string SQL)
+        {
+            int i = 0;
+            foreach (Object a in data)
+            {
+                SQL = SQL.Replace("{" + i + "}", "'" + a.ToString() + "'");
+                i++;
+            }
+            try
+            {
+                MysqlCom.CommandText = SQL;
+                MysqlCom.Connection = MysqlCon;
+                MysqlCon.Open();
+            //Console.WriteLine(SQL);
+            
+                if (MysqlCom.ExecuteNonQuery() > 0)
+                {
+                    MysqlCon.Close();
+                    return 1;
+                }
+                else
+                {
+                    MysqlCon.Close();
+                    return 0;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.ErrorCode);
+                throw;
+            }
+        }
+        //删除数据
+        public int Delete(string SQL)
+        {
+            return 1;
+        }
+    }
+}
---
+++ DateBaseLink/DateBaseLink/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateBaseLink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sql = "INSERT INTO user(username,userpassword) values({0},{1})";
+            string sql2 = "UPDATE user SET username={0},userpassword={1}where username='wyh'";
+            Object[] data = { "haha", "asdfgh" };
+            string sqllink = "Host =localhost;Database=socket;Username=root;Password=123456";
+            DataBaseLink dbl = new DataBaseLink(sqllink);
+            if(dbl.Updata(data, sql2)>0)
+            {
+                Console.WriteLine("成功");
+            }
+            else
+            {
+                Console.WriteLine("失败");
+            }
+        }
+    }
+}
